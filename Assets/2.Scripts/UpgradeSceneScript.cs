f023acce49a593b00b9d5e97023439ecca6d2825
KJYp <khkim8892@gmail.com>
2026-05-19 14:58:06 +0900

    1차 upgradeScene UI 개발

--- Assets/2.Scripts/UpgradeSceneScript.cs
+++ Assets/2.Scripts/UpgradeSceneScript.cs
@@ -1,8 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UpgradeSceneScript : MonoBehaviour
 {
+    public TMP_Text beforeUpgradeHPText;
+    public TMP_Text beforeUpgradeAPText;
+    public TMP_Text beforeUpgradeACText;
+    public TMP_Text beforeUpgradeMSText;
+    public TMP_Text beforeUpgradeMCText;
+    public TMP_Text beforeUpgradeSCText;
+
+    public TMP_Text afterUpgradeHPText;
+    public TMP_Text afterUpgradeAPText;
+    public TMP_Text afterUpgradeACText;
+    public TMP_Text afterUpgradeMSText;
+    public TMP_Text afterUpgradeMCText;
+    public TMP_Text afterUpgradeSCText;
+
+    public TMP_Text UnitDescriptionText;
+
+    public Image beforeAPImage;
+    public Image afterAPImage;
+    public Sprite[] APSprite;
 
     void Start()
     {
@@ -19,5 +40,49 @@ public class UpgradeSceneScript : MonoBehaviour
     public void BackBtnClick()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void SetUnitStat(int n)
+    {
+        if (n == 0)
+        {
+            beforeAPImage.sprite = APSprite[0];
+            afterAPImage.sprite = APSprite[0];
+        } 
+        if (n == 1)
+        {
+            beforeAPImage.sprite = APSprite[1];
+            afterAPImage.sprite = APSprite[1];
+        }
+    }
+
+    public void MeleeUnitBtnClick()
+    {
+        UnitDescriptionText.text = "컴공과 유닛입니다.";
+        SetUnitStat(0);
+    }
+
+    public void DamageUnitBtnClick()
+    {
+        UnitDescriptionText.text = "화학과 유닛입니다.";
+        SetUnitStat(0);
+    }
+
+    public void TankUnitBtnClick()
+    {
+        UnitDescriptionText.text = "글스산 유닛입니다.";
+        SetUnitStat(0);
+    }
+
+    public void RangedUnitBtnClick()
+    {
+        UnitDescriptionText.text = "기후학과 유닛입니다.";
+        SetUnitStat(0);
+    }
+    
+    public void HealerUnitBtnClick()
+    {
+        UnitDescriptionText.text = "통번역과 유닛입니다.";
+        SetUnitStat(1);
     }
 }
